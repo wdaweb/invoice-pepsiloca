bfdbf146a0f0a5ab0d6faaff6b5b97c8c60c1f3a
u8931011 <u8931011@gmail.com>
2020-05-14 11:57:20 +0800

    期數與版面美化

--- invoice-pepsiloca/list.php
+++ invoice-pepsiloca/list.php
@@ -1,3 +1,5 @@
+<?php include "./com/base.php";?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +9,57 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-<?php include "./include/header.php";?>
+<?php include "./include/header.php";
+
+// 計算月份轉為期
+$period=ceil(date("n")/2);
+// echo $period;
+if(isset($_GET['period'])){
+    $period=$_GET['period'];
+}
+
+
+?>
+
+
+<h1>發票列表</h1>
+<ul class="nav">
+<li><a href="list.php?period=1" style="background:<?=($period==1)?'pink':'white';?>">1(1,2)</a></li>
+<li><a href="list.php?period=2" style="background:<?=($period==2)?'pink':'white';?>">2(3,4)</a></li>
+<li><a href="list.php?period=3" style="background:<?=($period==3)?'pink':'white';?>">3(5,6)</a></li>
+<li><a href="list.php?period=4" style="background:<?=($period==4)?'pink':'white';?>">4(7,8)</a></li>
+<li><a href="list.php?period=5" style="background:<?=($period==5)?'pink':'white';?>">5(9,10)</a></li>
+<li><a href="list.php?period=6" style="background:<?=($period==6)?'pink':'white';?>">6(11,12)</a></li>
+</ul>
+
+
+<?php
+
+$sql="select * from invoice where period='$period'";
+$rows=$pdo->query($sql)->fetchAll();
+?>
+
+<table class="table">
+    <tr>
+        <td>編號</td>
+        <td>標記</td>
+        <td>號碼</td>
+        <td>花費</td>
+    </tr>
+    <?php
+    foreach($rows as $row){
+    ?>
+    <tr>
+        <td><?=$row['id'];?></td>
+        <td><?=$row['code'];?></td>
+        <td><?=$row['number'];?></td>
+        <td><?=$row['expend'];?></td>
+    </tr>
+    <?php
+    }
+    ?>
+</table>
+
     
 </body>
 </html>
